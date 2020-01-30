b55afd6de632fc7faf354cc8de9db9dc32828714
George Lewis <52492413+glewi@users.noreply.github.com>
2020-01-30 16:59:20 +0000

    Skeleton for the symbol table added.

---
+++ PlotlangConverter/SvgFrontend/SvgSymbolTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotlangConverter.SvgFrontend
+{
+    class SvgSymbolTable
+    {
+        private Dictionary<string, IToken> symbolMap = new Dictionary<string, IToken>();
+
+        public IToken GetToken(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToken(string identifier, IToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveToken(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal interface IToken
+    {
+
+    }
+
+    internal class RectangleToken : IToken
+    {
+
+    }
+}
