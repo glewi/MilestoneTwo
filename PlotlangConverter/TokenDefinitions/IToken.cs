da74f9f1e8db205f6325f485a6a0ee71194c1192
George Lewis <52492413+glewi@users.noreply.github.com>
2020-02-02 15:59:57 +0000

    Added getters and setters for token paramaters.

--- PlotlangConverter/TokenDefinitions/IToken.cs
+++ PlotlangConverter/TokenDefinitions/IToken.cs
@@ -9,27 +9,55 @@ namespace PlotlangConverter.TokenDefinitions
 
     public interface IToken
     {
-
+        object[] getParams();
+        void setParams(params object[] vs);
     }
 
-    internal class RectangleToken : IToken
+    public class RectangleToken : IToken
     {
         // XY coords for the bottom left corner of the rectangle.
-        Tuple<int, int> xy { get; } = new Tuple<int, int>(0,0);
+        Tuple<int, int> xy { get; }
 
         // XY values for rounded rectangles. 
-        Tuple<uint, uint> rxy { get; } = new Tuple<uint, uint>(0, 0);
+        Tuple<uint, uint> rxy { get; }
 
         // Rectangle width and height dimensions
-        Tuple<uint, uint> widthheight { get; } = new Tuple<uint, uint>(0,0);
+        Tuple<uint, uint> widthheight { get; }
+
+        public RectangleToken(int x, int y, uint rx, uint ry, uint width, uint height)
+        {
+            xy = new Tuple<int, int>(x, y);
+            rxy = new Tuple<uint, uint>(rx, ry);
+            widthheight = new Tuple<uint, uint>(width, height);
+        }
+
+        object[] getParams()
+        {
+            return null;
+        }
+        
+        void setParams(params object[] vs)
+        {
+
+        }
     }
 
-    internal class CircleToken : IToken
+    public class CircleToken : IToken
     {
         // XY coords for the center of the circle.
         public Tuple<int, int> xy { get; } = new Tuple<int, int>(0,0);
 
         // Circle radius.
         public uint r { get; } = 0;
+
+        object[] getParams()
+        {
+            return null;
+        }
+
+        void setParams(params object[] vs)
+        {
+
+        }
     }
 }
