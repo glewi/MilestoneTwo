a93c177e9cfcc473631c3ac7f10843300f17cbfa
George Lewis <52492413+glewi@users.noreply.github.com>
2020-02-04 20:40:53 +0000

    Slight workaround for the polymorphic serialization problem.

--- PlotlangConverter/SvgFrontend/TinySvgParser.cs
+++ PlotlangConverter/SvgFrontend/TinySvgParser.cs
@@ -5,10 +5,12 @@ using System.Text.Json;
 namespace PlotlangConverter.SvgFrontend
 {
     static class TinySvgParser
-    {
+    {   
         public static void Parse(IToken token)
         {
-            Console.WriteLine(token.GetTokenID());
+            Tuple<uint, object[]> tuple = new Tuple<uint, object[]>(token.GetTokenID(), token.GetParams());
+            Console.WriteLine(tuple.ToString());
+            Console.WriteLine(JsonSerializer.Serialize(tuple));
         }
     }
 }
