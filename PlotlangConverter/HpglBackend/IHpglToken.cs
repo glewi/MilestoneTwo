b99060e976721c950b2aed3346393df765bfce29
George Lewis <52492413+glewi@users.noreply.github.com>
2020-02-04 21:43:56 +0000

    Began implementing HpglToken classes.

--- PlotlangConverter/HpglBackend/IHpglToken.cs
+++ PlotlangConverter/HpglBackend/IHpglToken.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace PlotlangConverter.HpglBackend
 {
-    class IHpglToken
+    interface IHpglToken
     {
+        string GetCommand();
+    }
+
+    public class HpglRectangle : IHpglToken
+    {
+        public HpglRectangle(params object[] vs)
+        {
+
+        }
+        
+        public string GetCommand()
+        {
+            string cmd = "";
+            return cmd;
+        }
+    }
+
+    public class HpglCircle : IHpglToken
+    {
+        public string GetCommand()
+        {
+            string cmd = "";
+            return cmd;
+        }
     }
 }
