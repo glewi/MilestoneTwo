54102d0ef73dc1fe961247d0c0688e264d6029cf
George Lewis <52492413+glewi@users.noreply.github.com>
2020-02-11 18:24:08 +0000

    Started creating a custom converter.

--- PlotlangConverter/SvgFrontend/TinySvgParser.cs
+++ PlotlangConverter/SvgFrontend/TinySvgParser.cs
@@ -7,33 +7,17 @@ using System.Text.Json;
 namespace PlotlangConverter.SvgFrontend
 {
     static class TinySvgParser
-    {   
-        public class SerialisationObject
-        {
-            public UInt16 TokenID { get; set; }
-            object[] args { get; set; }
-
-            public SerialisationObject(uint TokenID, params object[] vs)
-            {
-                this.TokenID = (UInt16)TokenID;
-                args = new object[vs.Length];
-                args = vs;
-            }
-        }
-
+    {
         public static void Parse(IToken token)
         {
-            SerialisationObject serialisation = new SerialisationObject(token.GetTokenID(), token.GetParams());
-            string json = JsonSerializer.Serialize(serialisation);
-
-            IList<int> vs = new List<int>(  new int[] { 1,2,3,4,5} );
-            Console.WriteLine(JsonSerializer.Serialize(vs));
-
-            
-
             Dictionary<string, int[]> dic = new Dictionary<string, int[]>();
             dic.Add(token.GetTokenID().ToString(),new int[3] { 123,456,789});
             Console.WriteLine(JsonSerializer.Serialize(dic));
+
+            foreach (object arg in token.GetParams())
+            {
+                Console.WriteLine(arg.ToString());
+            }
 
         }
     }
